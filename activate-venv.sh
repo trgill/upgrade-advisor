0129f65c85bbc32d3c85d4f8fe6819726b69432c
Todd Gill <tgill@redhat.com>
2026-06-05 10:44:33 -0400

    add activate-venv.sh
    Signed-off-by: Todd Gill <tgill@redhat.com>

---
+++ activate-venv.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Activate virtual environment for upgrade-advisor
+source venv/bin/activate
+echo "✓ Virtual environment activated"
+echo "Python: $(which python3)"
+echo "To deactivate, run: deactivate"
